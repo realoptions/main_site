535a5fa0ca8e018c211adf7640848ae6328258ea
Daniel Stahl <B0GX3@regions.com>
2018-10-22 08:54:36 -0500

    improved layout and organization

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Main site for option model APIs
 
-More to come...
+This was copied from the [option_price_faas](https://github.com/phillyfan1138/option_price_faas/commit/45b67ee74a9e368ee8e038375875fd49eb2dff1c) at commit `45b67ee74a9e368ee8e038375875fd49eb2dff1c`.  
--- src/actions/subscriptions.js
+++ src/actions/subscriptions.js
@@ -1,50 +1,68 @@
 import {
-    ADD_SUBSCRIPTION, 
-    DELETE_SUBSCRIPTION, 
-    IS_UNREGISTERING,
-    UPDATE_CATALOG, 
-    NO_SUBSCRIPTION_ERROR,
-    SUBSCRIPTION_ERROR,
-    UPDATE_USAGE
+  ADD_SUBSCRIPTION,
+  DELETE_SUBSCRIPTION,
+  IS_UNREGISTERING,
+  UPDATE_CATALOG,
+  NO_SUBSCRIPTION_ERROR,
+  SUBSCRIPTION_ERROR,
+  UPDATE_USAGE
 } from './constants'
-import {unregisterPaid, getUsage, getCatalog} from '../services/api-catalog'
+import { unregisterPaid, getUsage, getCatalog } from '../services/api-catalog'
+import { keys } from '../reducers/catalog'
+const containsString = (match, string) => match.toLowerCase().includes(string)
+const checkKey = name => keys.find(key => containsString(name, key))
+export const addSubscriptionLocal = dispatch => usagePlanId =>
+  dispatch({
+    type: ADD_SUBSCRIPTION,
+    value: usagePlanId
+  })
 
-export const addSubscriptionLocal=dispatch=>usagePlanId=>dispatch({
-    type:ADD_SUBSCRIPTION,
-    value:usagePlanId
-})
+export const deleteSubscriptionLocal = dispatch => usagePlanId =>
+  dispatch({
+    type: DELETE_SUBSCRIPTION,
+    value: usagePlanId
+  })
 
-export const deleteSubscriptionLocal=dispatch=>usagePlanId=>dispatch({
-    type:DELETE_SUBSCRIPTION,
-    value:usagePlanId
-})
-
-export const removePaidSubscription=dispatch=>(paidUsagePlanId, freeUsagePlanId, client)=>{
-    dispatch({
-        type:IS_UNREGISTERING,
-        value:true
+export const removePaidSubscription = dispatch => (
+  paidUsagePlanId,
+  freeUsagePlanId,
+  client
+) => {
+  dispatch({
+    type: IS_UNREGISTERING,
+    value: true
+  })
+  unregisterPaid(paidUsagePlanId, freeUsagePlanId, client)
+    .then(() => {
+      addSubscriptionLocal(dispatch)(freeUsagePlanId)
+      deleteSubscriptionLocal(dispatch)(paidUsagePlanId)
+      dispatch({ type: NO_SUBSCRIPTION_ERROR })
     })
-    unregisterPaid(paidUsagePlanId, freeUsagePlanId, client)
-    .then(()=>{
-        addSubscriptionLocal(dispatch)(freeUsagePlanId)
-        deleteSubscriptionLocal(dispatch)(paidUsagePlanId)
-        dispatch({type:NO_SUBSCRIPTION_ERROR})
-    })
-    .catch(err=>dispatch({type:SUBSCRIPTION_ERROR, err}))
-    .then(()=>dispatch({
-            type:IS_UNREGISTERING,
-            value:false
-        })
+    .catch(err => dispatch({ type: SUBSCRIPTION_ERROR, err }))
+    .then(() =>
+      dispatch({
+        type: IS_UNREGISTERING,
+        value: false
+      })
     )
 }
 
-export const getSubscriptionUsage=dispatch=>(usagePlanId, client)=>getUsage(
-    usagePlanId, 
-    client
-)
-.then(({data})=>dispatch({type:UPDATE_USAGE, value:data}))
-.then(()=>dispatch({type:NO_SUBSCRIPTION_ERROR}))
-.catch(err=>dispatch({type:SUBSCRIPTION_ERROR, err}))
+export const getSubscriptionUsage = dispatch => (usagePlanId, client) =>
+  getUsage(usagePlanId, client)
+    .then(({ data }) => dispatch({ type: UPDATE_USAGE, value: data }))
+    .then(() => dispatch({ type: NO_SUBSCRIPTION_ERROR }))
+    .catch(err => dispatch({ type: SUBSCRIPTION_ERROR, err }))
 
-export const getPossibleSubscriptions=dispatch=>getCatalog()
-.then(value=>dispatch({type:UPDATE_CATALOG, value}))
+export const getPossibleSubscriptions = dispatch =>
+  getCatalog()
+    .then(arr =>
+      arr.reduce((aggr, curr) => {
+        const key = checkKey(curr.name)
+        if (key) {
+          return { ...aggr, [key]: curr }
+        } else {
+          return aggr
+        }
+      }, {})
+    )
+    .then(value => dispatch({ type: UPDATE_CATALOG, value }))
